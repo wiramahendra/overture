56d0adb172b9e00b94b8555ec6c5ddfe7ae77cab
Wira Mahendra <wiramahendra@proton.me>
2026-09-01 00:18:03 +0800

    chore: update api/operator_reconciliation_postgres_test.go

--- api/operator_reconciliation_postgres_test.go
+++ api/operator_reconciliation_postgres_test.go
@@ -11,8 +11,8 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/Igris-inertial/system/igris-overture/coordinator"
-	"github.com/Igris-inertial/system/igris-overture/middleware"
+	"github.com/wiramahendra/overture/coordinator"
+	"github.com/wiramahendra/overture/middleware"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
