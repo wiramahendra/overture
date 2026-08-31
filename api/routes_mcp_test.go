f4c8ac986de1b506a1aec1abe5e28a94569aa730
Wira Mahendra <wiramahendra@proton.me>
2026-09-01 00:18:12 +0800

    chore: update api/routes_mcp_test.go

--- api/routes_mcp_test.go
+++ api/routes_mcp_test.go
@@ -16,8 +16,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/Igris-inertial/system/igris-overture/coordinator"
-	"github.com/Igris-inertial/system/igris-overture/middleware"
+	"github.com/wiramahendra/overture/coordinator"
+	"github.com/wiramahendra/overture/middleware"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
