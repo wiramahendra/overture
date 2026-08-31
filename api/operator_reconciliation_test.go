0e49bf352fa38723ce3750e9d6ade91966181728
Wira Mahendra <wiramahendra@proton.me>
2026-09-01 00:18:04 +0800

    chore: update api/operator_reconciliation_test.go

--- api/operator_reconciliation_test.go
+++ api/operator_reconciliation_test.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/Igris-inertial/system/igris-overture/coordinator"
-	"github.com/Igris-inertial/system/igris-overture/middleware"
+	"github.com/wiramahendra/overture/coordinator"
+	"github.com/wiramahendra/overture/middleware"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
