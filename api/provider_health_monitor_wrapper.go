e44d60518f2e30d67a5ae485216440c459d2cfc1
Wira Mahendra <wiramahendra@proton.me>
2026-09-01 00:18:06 +0800

    chore: update api/provider_health_monitor_wrapper.go

--- api/provider_health_monitor_wrapper.go
+++ api/provider_health_monitor_wrapper.go
@@ -1,11 +1,16 @@
+//go:build ignore
+// +build ignore
+
+// Archived: scheduler provider health monitor wrapper (attic/scheduler). Not in wedge.
+
 // Package api provides wrappers for scheduler components
 package api
 
 import (
 	"database/sql"
 
-	"github.com/Igris-inertial/system/igris-overture/scheduler"
-	"github.com/Igris-inertial/system/igris-overture/security"
+	"github.com/wiramahendra/overture/scheduler"
+	"github.com/wiramahendra/overture/security"
 )
 
 // NewProviderHealthMonitor creates a new provider health monitor
