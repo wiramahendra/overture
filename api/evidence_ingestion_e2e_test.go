081320a27b12454929c6b47166a8063ad1d745b1
Wira Mahendra <wiramahendra@proton.me>
2026-09-01 00:18:01 +0800

    chore: update api/evidence_ingestion_e2e_test.go

--- api/evidence_ingestion_e2e_test.go
+++ api/evidence_ingestion_e2e_test.go
@@ -33,8 +33,8 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/require"
 
-	"github.com/Igris-inertial/system/igris-overture/internal/canonicaljson"
-	"github.com/Igris-inertial/system/igris-overture/security"
+	"github.com/wiramahendra/overture/internal/canonicaljson"
+	"github.com/wiramahendra/overture/security"
 )
 
 const evidenceJournalScript = `
