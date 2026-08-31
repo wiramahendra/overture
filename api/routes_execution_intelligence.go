d6751eccb7df1d38093cd6bfe4665665b6a0d192
Wira Mahendra <wiramahendra@proton.me>
2026-09-01 00:18:10 +0800

    chore: update api/routes_execution_intelligence.go

--- api/routes_execution_intelligence.go
+++ api/routes_execution_intelligence.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 
-	"github.com/Igris-inertial/system/igris-overture/middleware"
+	"github.com/wiramahendra/overture/middleware"
 )
 
 // RegisterExecutionIntelligenceRoutes exposes read-only operational metrics
